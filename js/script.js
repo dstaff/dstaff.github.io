1754c5a78506ca997759b113bd79cd65a431d99f
dstaff <danis.staff@gmail.com>
2023-07-17 19:21:56 +0000

    reducing text size while loading

--- js/script.js
+++ js/script.js
@@ -146,7 +146,7 @@ loadQuestion();
     function loading()
     {
       $('.submit-button').attr("disabled","disabled");
-      $('.submit-button').html("<i class='fa fa-circle-o-notch fa-pulse'></i> Validando Respuesta...");
+      $('.submit-button').html("<i class='fa fa-circle-o-notch fa-pulse'></i> Validando");
     }
 
     function stop_loading()
